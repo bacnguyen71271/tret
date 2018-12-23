47b474c5b38a1f35afd530296c7893ac4d450761
vuly <phuongoanh0795@gmail.com>
2018-12-23 19:18:17 +0700

    Update

--- ssl.js
+++ ssl.js
@@ -1,9 +1,10 @@
 const express = require('express');
+var serveIndex = require('serve-index');
 
 // Configure & Run the http server
 const app = express();
+app.use('/.well-known', express.static('.well-known'), serveIndex('.well-known'));
 
-app.get('.well-known/acme-challenge/5FKx4lpLudCx-qaUcUpRxjmZNoLkNiG-eT4WKUoGt5Q', function (req, res) { res.send('5FKx4lpLudCx-qaUcUpRxjmZNoLkNiG-eT4WKUoGt5Q.zVSWIvmchnx5iyynm0EVIfGQNL-JdW4TlsHPwbtReJA'); });
 app.listen(2000, () => {
   console.log('HTTP server running on port 80');
 });
