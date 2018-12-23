134d5a1014d35fe002404e0c0d0527b872ab4231
vuly <phuongoanh0795@gmail.com>
2018-12-23 20:55:21 +0700

    Update

--- ssl.js
+++ ssl.js
@@ -1,9 +1,9 @@
 const express = require('express');
-var serveIndex = require('serve-index');
 
 // Configure & Run the http server
 const app = express();
-app.use('/.well-known', express.static('.well-known'), serveIndex('.well-known'));
+
+app.use(express.static(__dirname + '/static', { dotfiles: 'allow' } ))
    
 app.listen(2000, () => {
   console.log('HTTP server running on port 80');
