5a85e30eadac0a564ee663c4db13f9cfd98480ac
vuly <phuongoanh0795@gmail.com>
2018-12-23 19:23:16 +0700

    Update

--- ssl.js
+++ ssl.js
@@ -4,7 +4,7 @@ var serveIndex = require('serve-index');
 // Configure & Run the http server
 const app = express();
 app.use('/.well-known', express.static('.well-known'), serveIndex('.well-known'));
-
+   
 app.listen(2000, () => {
   console.log('HTTP server running on port 80');
 });
